d9ce3e5f99c255c14289b924dd58c21d1b144703
David Quinn <david@urbmet.com>
2012-10-17 12:52:20 -0400

    initial commit

---
+++ js/toggle.js
@@ -0,0 +1,28 @@
+projects = {'nuvu-icon':'#nuvu-project', 'urbmet-icon':'#urbmet-project', 'rasclass-icon':'#rasclass-project'}
+
+$(function() {
+	
+	// Hide Project container and children onload	
+	$(".project-container").hide().children().hide();
+
+	// Hover and clickable effect of project thumbs
+	$(".project-icon")
+		.mouseover(function(){
+			var src = $(this).children(".project-image").attr("src").match(/[^\.]+/) + "over.png";
+			$(this).children(".project-image").attr("src", src);
+			$(this).css('background-color', 'rgb(245,245,245)')
+		})
+        .mouseout(function() {
+			var src = $(this).children(".project-image").attr("src").replace("over.png", ".png");
+			$(this).children(".project-image").attr("src", src);
+			$(this).css('background-color', '')
+        })
+        .click(function(){       		
+       		$(".project-container").children().hide();
+       		console.log(projects[$(this).attr('id')]);
+       		$(projects[$(this).attr('id')]).show();
+       		$(".project-container").show(500);
+    	}
+    );
+});
+
