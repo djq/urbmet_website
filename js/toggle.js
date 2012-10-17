b416dd479804bcf014b6208d3da18f6a26573ca7
David Quinn <david@urbmet.com>
2012-10-17 14:51:55 -0400

    tweaked a lot of things; made sure all styles were in .css; experimented with a few different gray shades; made boxes a little bigger. Changed several of the classes that were only used once into #IDs.

--- js/toggle.js
+++ js/toggle.js
@@ -10,12 +10,12 @@ $(function() {
 		.mouseover(function(){
 			var src = $(this).children(".project-image").attr("src").match(/[^\.]+/) + "over.png";
 			$(this).children(".project-image").attr("src", src);
-			$(this).css('background-color', 'darkgray')
+			$(this).css('background-color', '#363636')
 		})
         .mouseout(function() {
 			var src = $(this).children(".project-image").attr("src").replace("over.png", ".png");
 			$(this).children(".project-image").attr("src", src);
-			$(this).css('background-color', 'gray')
+			$(this).css('background-color', '#4F4F4F')
         })
         .click(function(){       		
        		$(".project-container").children().hide();
